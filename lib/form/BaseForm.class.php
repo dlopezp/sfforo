001435273372091be7ff70a6408f692b5a23601e
Daniel López <dlopezp@gmx.es>
2013-04-12 19:34:45 +0200

    Primer commit

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    sfforo
+ * @subpackage form
+ * @author     Daniel López 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
