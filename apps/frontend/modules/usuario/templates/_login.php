0425b19bacc28553d5fa5307d4350078ee7f3ebd
Daniel López Pérez <dlopez@dlopez-fct.(none)>
2013-04-13 21:04:28 +0200

    Añadido estilos al login

--- apps/frontend/modules/usuario/templates/_login.php
+++ apps/frontend/modules/usuario/templates/_login.php
@@ -1,10 +1,25 @@
+
+
 <div id="div-login">
   <form class="well form-inline">
-    <input type="text" class="input-small" placeholder="Username">
-    <input type="password" class="input-small" placeholder="Password">
-    <label class="checkbox">
-      <input type="checkbox"> Recordar
-    </label>
-    <button type="submit" class="btn">Entrar</button>
+    <div class="div-login-inicio">
+      <button type="submit" class="btn btn-primary">Regístrate</button>
+      <button type="submit" class="btn btn-warning">Idéntificate</button>
+    </div>
+    <div class="div-login-entrar">
+      <input type="text" class="input-small" placeholder="Username">
+      <input type="password" class="input-small" placeholder="Password">
+      <button type="submit" class="btn btn-success">Entrar</button>
+      <br>
+      <label class="checkbox">
+        <input type="checkbox"> Recordar
+      </label>
+    </div>
   </form>
+  <div class="div-login-identificado">
+    Nombre Completo (username) 
+    <br>
+    <a href="#" class="btn btn-mini btn-success">Panel de control</a>
+    <a href="#" class="btn btn-mini btn-danger">Cerrar sesion</a>
+  </div>
 </div>
