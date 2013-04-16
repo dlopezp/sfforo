a743e965cb1d0d2ddb422f4471b8acc248f47092
Daniel López <dlopezp@gmx.es>
2013-04-16 18:44:03 +0200

    Login completo

--- apps/frontend/modules/usuario/templates/_login.php
+++ apps/frontend/modules/usuario/templates/_login.php
@@ -1,6 +1,6 @@
 <div class="div-login-identificado">
-  Nombre Completo (username)
+  <?php echo $sf_user->getName().' ('.$sf_user->getUsername().')'; ?>
   <br>
-  <a href="#" class="btn btn-mini btn-success">Panel de control</a>
-  <a href="#" class="btn btn-mini btn-danger">Cerrar sesion</a>
+  <?php echo link_to('Panel de control', '@homepage', array('class' => 'btn btn-mini btn-success')) ?>
+  <?php echo link_to('Cerrar sesion', '@sf_guard_signout', array('class' => 'btn btn-mini btn-danger')) ?>
 </div>
