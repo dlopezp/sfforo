eeab8d9fad99c4d8d1b2d872bc8530f3b3bc8c10
Daniel López Pérez <dlopez@dlopez-fct.(none)>
2013-04-17 11:19:05 +0200

    Cambiado bootstrap de min a normal y la ruta de las imagenes

--- apps/frontend/modules/usuario/templates/_login.php
+++ apps/frontend/modules/usuario/templates/_login.php
@@ -1,6 +1,6 @@
 <div class="div-login-identificado">
   <?php echo $sf_user->getName().' ('.$sf_user->getUsername().')'; ?>
   <br>
-  <?php echo link_to('Panel de control', '@homepage', array('class' => 'btn btn-mini btn-success')) ?>
-  <?php echo link_to('Cerrar sesion', '@sf_guard_signout', array('class' => 'btn btn-mini btn-danger')) ?>
+  <a href="<?php echo url_for('@homepage'); ?>" class="btn btn-mini btn-success"><i class="icon-user icon-white"></i> Panel de control</a>
+  <a href="<?php echo url_for('@sf_guard_signout'); ?>" class="btn btn-mini btn-danger"><i class="icon-remove-sign icon-white"></i> Cerrar sesión</a>
 </div>
