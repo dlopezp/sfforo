90dae6d3e97fd94d89c0df74f881385836f42360
Daniel López Pérez <daniel.lopez@blinklearning.com>
2014-03-11 17:44:15 +0100

    Arreglando

--- web/index.php
+++ web/index.php
@@ -1,7 +1,5 @@
 <?php
 
-$FOO2;
-$pull_request;
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
