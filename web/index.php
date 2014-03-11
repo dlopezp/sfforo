34753fbaeec06734b5a3c8ab26fe5ebbe0e7baea
Daniel López Pérez <daniel.lopez@blinklearning.com>
2014-03-11 16:44:40 +0100

    fgbsfb

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 
 $FOO2;
+$pull_request;
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
