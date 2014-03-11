ad9a55539b1ab21be22e982f63c116f13a53dc57
Daniel López Pérez <daniel.lopez@blinklearning.com>
2014-03-11 16:40:19 +0100

    yujty

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-
+nhgdmhgdhmgd
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
